2e1f6ab51b91c2a62b8d6ecc8d397cac5f5670da
ita5478 <ittai.paz@gmail.com>
2022-08-31 16:01:26 +0300

    Add ITokenizer and basic implementation.

---
+++ ConsoleApp1/Abstractions/ITokenizer.cs
@@ -0,0 +1,9 @@
+﻿using Calculator.BL;
+
+namespace ConsoleApp1.Abstractions
+{
+    public interface ITokenizer
+    {
+        Token Tokenize(string token);
+    }
+}
---
+++ ConsoleApp1/Implementations/Tokenizer.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.BL;
+using Calculator.BL.Enums;
+using ConsoleApp1.Abstractions;
+
+namespace ConsoleApp1.Implementations
+{
+    public class Tokenizer : ITokenizer
+    {
+        public Token Tokenize(string token)
+        {
+            return new Token(token, TokenType.Number);
+        }
+    }
+}
