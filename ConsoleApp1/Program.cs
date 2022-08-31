f6a044e588b8aa07f1d0fd549ab285b4c8370b7f
ita5478 <ittai.paz@gmail.com>
2022-08-31 14:05:31 +0300

    Add project refernce in console app.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculator.Core.Implementations;
+
+var number = new CalculatableNumber(5);
+Console.WriteLine(number.Calculate());
