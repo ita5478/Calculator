3c6df189dbcde151cdd755791962ec1e48bfbd7e
ita5478 <ittai.paz@gmail.com>
2022-08-31 14:14:44 +0300

    Add usage of addition in main.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Calculator.Core.Implementations;
+using Calculator.Core.Implementations.BinaryOperations;
 
 var number = new CalculatableNumber(5);
-Console.WriteLine(number.Calculate());
+var secondNumber = new CalculatableNumber(3);
+var addition = new Addition(number, secondNumber);
+Console.WriteLine(addition.Calculate());
