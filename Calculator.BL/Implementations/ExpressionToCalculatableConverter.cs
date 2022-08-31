a96efcb980495893bc62bcc82e2214a6e2c0ce32
ita5478 <ittai.paz@gmail.com>
2022-08-31 22:09:37 +0300

    Implement token expression to Calculateable converter.

---
+++ Calculator.BL/Implementations/ExpressionToCalculatableConverter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using Calculator.BL.Enums;
+using Calculator.Common.Abstractions;
+using Calculator.Core.Abstractions;
+using Calculator.Core.Implementations;
+
+namespace Calculator.BL.Implementations
+{
+    public class ExpressionToCalculatableConverter : IConverter<IList<Token>, ICalculatable>
+    {
+        private readonly ITransformer<IList<Token>> _transformer;
+        private readonly IDictionary<string, IBinaryOperationFactory> _binaryOperationsFactories;
+        public ExpressionToCalculatableConverter(
+            ITransformer<IList<Token>> shuntingYardTransformer,
+            IDictionary<string, IBinaryOperationFactory> binaryOperationsFactories)
+        {
+            _transformer = shuntingYardTransformer;
+            _binaryOperationsFactories = binaryOperationsFactories;
+        }
+
+        public ICalculatable Convert(IList<Token> from)
+        {
+            var transformedTokens = _transformer.Transform(from);
+            var operands = new Stack<ICalculatable>();
+            foreach (var token in transformedTokens)
+            {
+                switch (token.Type)
+                {
+                    case TokenType.Number:
+                        float number = float.Parse(token.Value);
+                        operands.Push(new CalculatableNumber(number));
+                        break;
+                    case TokenType.BinaryOperation:
+                        var factory = _binaryOperationsFactories[token.Value];
+                        var firstOperand = operands.Pop();
+                        var secondOperand = operands.Pop();
+                        operands.Push(factory.Create(firstOperand, secondOperand));
+                        break;
+                }
+            }
+
+            return operands.Pop();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using Calculator.BL.Implementations;
 using Calculator.Core.Abstractions;
 using Calculator.Core.Implementations.BinaryOperationFactories;
@@ -21,9 +22,9 @@ var numbersValidator = new NumbersValidator();
 var tokenizer = new Tokenizer(numbersValidator, binaryOperations.Keys.ToList(), brackets);
 var parser = new ExpressionParser(tokenizer);
 var transformer = new ShuntingYardTransformer();
-
+var expressionConverter = new ExpressionToCalculatableConverter(transformer, binaryOperations);
 string expression = "[3+4(5 /2)+4.5]";
-string ex = "5+3";
+string ex = "5+3+2";
 var tokenExpression = parser.Parse(ex).ToList();
-var result = transformer.Transform(tokenExpression);
-var a = 5;
+var result = expressionConverter.Convert(tokenExpression).Calculate();
+Console.WriteLine(result);
