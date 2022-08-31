3618152ae5fe90b3d6fc9f8fee8751e316322547
ita5478 <ittai.paz@gmail.com>
2022-08-31 16:09:03 +0300

    Add IValidator as common intrface and NumbersValidator,

---
+++ ConsoleApp1/Implementations/NumbersValidator.cs
@@ -0,0 +1,12 @@
+﻿using Calculator.Common.Abstractions;
+
+namespace ConsoleApp1.Implementations
+{
+    public class NumbersValidator : IValidator<string>
+    {
+        public bool Validate(string value)
+        {
+            return float.TryParse(value, out _);
+        }
+    }
+}
