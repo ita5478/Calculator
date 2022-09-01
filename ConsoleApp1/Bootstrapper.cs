e74f17977048d24e52706f28b9b8d70e8cdb9484
ita5478 <ittai.paz@gmail.com>
2022-09-01 13:08:18 +0300

    Add boostrapper.

---
+++ ConsoleApp1/Bootstrapper.cs
@@ -0,0 +1,40 @@
+﻿using Calculator.BL.Implementations;
+using Calculator.Core.Abstractions;
+using Calculator.Core.Implementations.BinaryOperationFactories;
+using CalculatorUI.Implementations;
+
+namespace ConsoleApp1
+{
+    public class Bootstrapper
+    {
+        public CalculatorUi Initialize()
+        {
+            var binaryOperations = new Dictionary<string, IBinaryOperationFactory>()
+            {
+                {"+", new AdditionFactory()},
+                {"*", new MultiplicationFactory()},
+                {"/", new DivisionFactory()},
+            };
+
+            var operationsPrecedence = binaryOperations.ToDictionary(
+                item => item.Key,
+                item => item.Value as IOperationPrecedence);
+
+            var brackets = new Dictionary<string, string>()
+            {
+                { "(", ")" },
+                { "[", "]" },
+            };
+
+            var numbersValidator = new NumbersValidator();
+
+            var tokenizer = new Tokenizer(numbersValidator, binaryOperations.Keys.ToList(), brackets);
+            var parser = new ExpressionParser(tokenizer);
+            var transformer = new ShuntingYardTransformer(operationsPrecedence);
+            var expressionConverter = new ExpressionToCalculatableConverter(transformer, binaryOperations);
+            var calculator = new CalculatorUi(parser, expressionConverter);
+
+            return calculator;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,36 +1,11 @@
-﻿using System.Linq.Expressions;
-using System.Text.RegularExpressions;
-using Calculator.BL.Implementations;
-using Calculator.Core.Abstractions;
-using Calculator.Core.Implementations.BinaryOperationFactories;
-using Calculator.Core.Implementations.BinaryOperations;
-using CalculatorUI.Implementations;
-using ConsoleApp1.Implementations;
+﻿using ConsoleApp1;
 
-var binaryOperations = new Dictionary<string, IBinaryOperationFactory>()
+var booter = new Bootstrapper();
+var calculator = booter.Initialize();
+
+while (true)
 {
-    {"+", new AdditionFactory()},
-    {"*", new MultiplicationFactory()},
-    {"/", new DivisionFactory()},
-};
-
-var operationsPrecedence = binaryOperations.ToDictionary(
-    item => item.Key, 
-    item => item.Value as IOperationPrecedence);
-
-var brackets = new Dictionary<string, string>()
-{
-    { "(", ")" },
-    { "[", "]" },
-};
-
-var numbersValidator = new NumbersValidator();
-
-var tokenizer = new Tokenizer(numbersValidator, binaryOperations.Keys.ToList(), brackets);
-var parser = new ExpressionParser(tokenizer);
-var transformer = new ShuntingYardTransformer(operationsPrecedence);
-var expressionConverter = new ExpressionToCalculatableConverter(transformer, binaryOperations);
-string expression = "[3+4(5 /2)+4.5]";
-string ex = "5*(3+2)/2";
-var calculator = new CalculatorUi(parser, expressionConverter);
-Console.WriteLine(calculator.Solve(expression));
+    Console.WriteLine("Enter expression:");
+    var expression = Console.ReadLine();
+    Console.WriteLine(calculator.Solve(expression));
+}
